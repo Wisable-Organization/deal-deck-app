a5c9814c1cb33affd99feb73066f9af32c0987cf
jo-sanchez-gbh <jo.sanchez@gbh.tech>
2025-11-06 18:18:35 -0400

    Added URL swapping for backend hosts

--- vite.config.ts
+++ vite.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
     ],
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: process.env.FASTAPI_API_URL || "http://127.0.0.1:8000",
         changeOrigin: true,
       },
     },
